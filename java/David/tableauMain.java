90a289188b6391638b068a623e7109be7437a8d4
David Dehoust <dehoustdavid@gmail.com>
2022-09-08 16:07:48 +0200

    ajout de tableauMain.java

---
+++ java/David/tableauMain.java
@@ -0,0 +1,9 @@
+public class tableauMain {
+
+    //tapper "java tableauMain.java (prenom) (animal pref.)" dans la console cmd.
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour " + args[0] + ".");
+        System.out.println("mon animal préféré est " + args[1] + ".");
+    }
+}
