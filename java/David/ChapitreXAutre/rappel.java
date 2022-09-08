d71fda915b7f18bed0b336e8e83022e6a5eca0ea
David Dehoust <dehoustdavid@gmail.com>
2022-09-08 15:40:56 +0200

    point 8 de l'excercice de rappel

--- java/David/ChapitreXAutre/rappel.java
+++ java/David/ChapitreXAutre/rappel.java
@@ -1,8 +1,12 @@
 package ChapitreXAutre;
 
+import java.text.DecimalFormat;
 import common.userInput;
 
 public class rappel {
+
+    static DecimalFormat format1 = new DecimalFormat("#.#");
+    static DecimalFormat format2 = new DecimalFormat("#.##");
 
     public static void main(String[] args) {
 
@@ -21,29 +25,29 @@ public class rappel {
 
         while (userInput.AskYesOrNo("Voulez vous calculer un IMC ?")) {
 
-            //factoriser la demande de taille et de poids
+            //il faut factoriser la demande de taille et de poids
 
             //demande de la taille.
             do {
                 height = userInput.AskFloat("Veuillez entrer votre taille en mètre (x,xx).");
                 if (height < heightMin || height > heightMax) {
-                    System.out.println(height + "m est une taille particulièrement improbable pour un adulte.");
+                    System.out.println(format2.format(height) + "m est une taille particulièrement improbable pour un adulte.");
                 }
             } while (height < heightMin || height > heightMax);
-            System.out.println("Vous mesurez " + height + " mètre");
+            System.out.println("Vous mesurez " + format2.format(height) + " mètre");
 
             //demande du poids.
             do {
                 weight = userInput.AskFloat("Veuillez entrer votre poids en kilogramme.");
                 if (weight < weightMin || weight > weightMax) {
-                    System.out.println(weight + "kg est un poids particulièrement improbable pour un adulte.");
+                    System.out.println(format1.format(weight) + "kg est un poids particulièrement improbable pour un adulte.");
                 }
             } while (weight < weightMin || weight > weightMax);
-            System.out.println("Vous pesez " + weight + " kilo");
+            System.out.println("Vous pesez " + format1.format(weight) + " kilos");
 
             //calcul de l'imc
             imc = IMCCalculation(height, weight);
-            System.out.println("\nVotre IMC est : " + imc);
+            System.out.println("\nVotre IMC est : " + format1.format(imc));
 
             //rapport et conseil
             IMCEvaluation(imcAll, imc);
@@ -69,21 +73,17 @@ public class rappel {
     }
 
     static String IMCRectification(float[] _imcAll, float _imc, float _taille, float _poids) {
-
         float _idealMin = 20 * (float) Math.pow(_taille, 2);
         float _idealMax = 25 * (float) Math.pow(_taille, 2);
-        float _weightRectification = 0.0f;
+        float[] _weightRectificationPlus = {_idealMin - _poids, _idealMax - _poids};
+        float[] _weightRectificationMinus = {_poids - _idealMin, _poids - _idealMax};
 
         if (_imc <= _imcAll[0]) {
-            _weightRectification = _idealMin - _poids;
-            return "Votre poids idéal est compris entre " + _idealMin + " et " + _idealMax + ".\nVous devriez idéalement prendre au moins " + _weightRectification + "kg.";
-
+            return "Votre poids idéal est compris entre " + format1.format(_idealMin) + " et " + format1.format(_idealMax) + ".\nVous devriez idéalement prendre entre " + format1.format(_weightRectificationPlus[0]) + " et " + format1.format(_weightRectificationPlus[1])  + "kg.";
         } else if (_imc >= _imcAll[1]) {
-            _weightRectification = _poids - _idealMax;
-            return "Votre poids idéal est compris entre " + _idealMin + " et " + _idealMax + ".\nVous devriez idéalement perdre au moins " + _weightRectification + "kg.";
-
+            return "Votre poids idéal est compris entre " + format1.format(_idealMin) + " et " + format1.format(_idealMax) + ".\nVous devriez idéalement perdre entre " + format1.format(_weightRectificationMinus[1]) + " et " + format1.format(_weightRectificationMinus[0]) + "kg.";
         } else {
-            return "Votre poids idéal est compris entre " + _idealMin + " et " + _idealMax + ".\nVous avez un poids adapté.";
+            return "Votre poids idéal est compris entre " + format1.format(_idealMin) + " et " + format1.format(_idealMax) + ".\nVous avez un poids adapté.";
         }
     }
 }
