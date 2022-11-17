ce65594e366e1e3b33c7a77ddb58a57eeb30509f
David Dehoust <dehoustdavid@gmail.com>
2022-11-17 13:53:56 +0100

    c8e1 et c8e2

--- java/David/Chapitre8Tableau/c8e1.java
+++ java/David/Chapitre8Tableau/c8e1.java
@@ -1,6 +1,74 @@
+import java.util.Scanner;
+
 public class c8e1 {
 
+    static Scanner uInput = new Scanner(System.in);
+
     public static void main(String[] args) {
+
+        String[] film = { "Jurassic Park", "Seigneur des Anneaux", "La Ligne Verte", "Les Simpsons : Le Film", "Old Boy" };
+
+        ShowArray(film);
+
+        while (AskYesOrNo("Voulez vous intervertir deux film ?")) {
+
+            int position1 = AskInt("position une ?") - 1;
+            int position2 = AskInt("position deux ?") - 1;
+
+            String temp = film[position1];
+            film[position1] = film[position2];
+            film[position2] = temp;
+
+            System.out.println("");
+
+            ShowArray(film);
+
+        }
+
+    }
+
+    public static void ShowArray(String[] _array){
+
+        for (int i = 0; i < _array.length; i++) {
+            System.out.println(_array[i]);
+        }
+
+    }
+
+    public static int AskInt(String _Question) {
+
+        int _Responce = 0;
+
+        while (true) {
+            System.out.print("\n" + _Question + "\nVotre Réponse : ");
+            if (uInput.hasNextInt()) {
+                _Responce = uInput.nextInt();
+                uInput.nextLine();
+                return _Responce;
+            } else {
+                System.err.println("Erreur, veuillez entrer un chiffre.");
+                uInput.nextLine();
+            }
+        }
+    }
+
+    public static boolean AskYesOrNo(String _Question) {
+
+        String _Responce = "";
         
+        while (true) {
+            System.out.print("\n" + _Question + " (o/n)\nVotre Réponse : ");
+            _Responce = uInput.nextLine().toUpperCase();
+
+            switch (_Responce) {
+                case "O":
+                    return true;
+                case "N":
+                    return false;
+                default:
+                    System.err.println("Erreur, votre réponse doit être \"o\" ou \"n\"");
+                    continue;
+            }
+        }
     }
 }
--- java/David/Chapitre8Tableau/c8e2.java
+++ java/David/Chapitre8Tableau/c8e2.java
@@ -1,5 +1,140 @@
+import java.util.Scanner;
+
 public class c8e2 {
+
+    static Scanner uInput = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
+        int noteMax = 10;
+        String newNote = "";
+        float[] notes = {};
+
+        System.out.println("Bonjour, veuillez spécifier votre note maximale");
+        noteMax = uInput.nextInt();
+        uInput.nextLine();
+
+        do {
+
+            newNote = AskNote("Veuillez entrez une note (tapez stop pour terminer)");
+
+            if (CheckIfNumeric(newNote)) {
+
+                float newNoteParsed = Float.parseFloat(newNote);
+
+                if (newNoteParsed < 0 || newNoteParsed > noteMax) {
+
+                    System.out.println("Votre note doit être supérieure à 0 et inférieure à la note maximale (" + noteMax + ").");
+
+                } else {
+
+                    System.out.println("Vous avez entré " + newNoteParsed + "/" + noteMax + ".\n");
+
+                    notes = ArrayAdd(notes, newNoteParsed);
+
+                    ShowArrayFloat(notes);
+
+                }
+
+            } else if (!(newNote.toUpperCase().equals("STOP"))) {
+
+                System.out.println("Erreur, veuillez entrer un chiffre ou nombre en guise de note.");
+
+            }
+
+        } while (!(newNote.toUpperCase().equals("STOP")));
+
+        if (CalculAverage(notes, noteMax) >= 80) {
+
+            System.out.println("Le contrôle est réussis.");
+
+        } else {
+
+            System.out.println("le contrôle doit être annulé.");
+
+        }
+
+        System.out.println("\nAu revoir.");
+
     }
+
+    public static String AskNote(String _question){
+
+        System.out.println(_question);
+
+        String _responce = uInput.nextLine();
+
+        return _responce;
+
+    }
+
+    public static boolean CheckIfNumeric(String _string){
+
+        try {
+
+            Float.parseFloat(_string);
+
+            return true;
+
+        } catch (Exception e) {
+
+            return false;
+
+        }
+
+    }
+
+    public static float[] ArrayAdd(float[] _array, float _valueToAdd){
+
+        float[] _newArray = new float[_array.length + 1];
+
+        if (_array.length > 0) {
+
+            for (int i = 0; i < _newArray.length - 1; i++) {
+
+                _newArray[i] = _array[i];
+
+            }
+
+            _newArray[_newArray.length - 1] = _valueToAdd;
+
+        } else {
+
+            _newArray[0] = _valueToAdd;
+
+        }
+
+        return _newArray;
+
+    }
+
+    public static void ShowArrayFloat(float[] _array){
+
+        for (int i = 0; i < _array.length; i++) {
+
+            System.out.println(_array[i]);
+
+        }
+
+    }
+
+    public static float CalculAverage(float[] _array, int _noteMax) {
+
+        float _sum = 0.0f;
+        float _percent = 0.0f;
+
+        for (int i = 0; i < _array.length; i++) {
+
+            _sum += _array[i];
+            // équivalent de : _sum = _sum + _array[i];
+
+        }
+
+        _percent = (_sum / _array.length) / _noteMax * 100;
+
+        System.out.println(_percent + "%");
+
+        return _percent;
+
+    }
+
 }
