cad7429404bda85aca0989968f9cbfb38478fed9
David Dehoust <dehoustdavid@gmail.com>
2022-11-23 12:53:55 +0100

    ras

---
+++ DB/Exercice26.sql
@@ -0,0 +1,24 @@
+/*
+Exercice n°26 - CREATE DATABASE / CREATE TABLE
+
+Allez dans le répertoire d'exercices SQL
+Connectez-vous au SGBD MySQL: mysql -u root -p
+Entrez votre mot de passe.
+Créez une base de données nommée: Exercice26
+Créer une Table Equipe. Cette table aura les champs suivants:
+IdEquipe (clef primaire): Entier, non null, non signé, auto-incrémenté.
+NomClub: chaîne de max 30 caractères, non null.
+Localité: chaîne de max 30 caractères, non null.
+Division: TINYINT non signé, non null.
+*/
+
+CREATE DATABASE Exercice26;
+
+USE Exercice26;
+
+CREATE TABLE Equipe (
+    IdEquipe PRIMARY KEY INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    NomClub VARCHAR(30) NOT NULL,
+    Localité(30) NOT NULL,
+    Division TINYINT UNSIGNED NOT NULL,
+);
