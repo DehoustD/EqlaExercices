46e8d2b287cd47c690c78403815e3f896d39bada
David Dehoust <dehoustdavid@gmail.com>
2022-12-07 12:43:25 +0100

    ras

---
+++ javaObjet/introduction/Animal.java
@@ -0,0 +1,20 @@
+public class Animal {
+    // au moins 6 propriétés
+    // au moins 2 methodes
+
+    // nom - String
+    // age - int
+    // sexe - String
+
+    // espèce - String
+    // eco-système - String
+    // sterilisé ? - boolean
+    // alimentation - String
+    // exotique ? - boolean
+    // nombre de membre - int
+    // heure de sommeil - int
+
+    // dormir meth
+    // manger meth
+
+}
