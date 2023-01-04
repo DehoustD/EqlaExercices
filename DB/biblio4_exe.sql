224dd3c557af76de5835bc3a4e9b5979c43daf6d
David <dehoustdavid@gmail.com>
2023-01-04 12:01:44 +0100

    biblio4 exe 5

--- DB/biblio4_exe.sql
+++ DB/biblio4_exe.sql
@@ -1,14 +1,5 @@
 USE biblio4_prof;
 
--- SELECT titre, theme.theme
--- FROM livre
--- WHERE id <= 20
--- INNER JOIN theme ON livre.theme_id = theme.id
--- ;
-
--- erreur 1 : WHERE Après INNER JOIN
--- erreur 2 : préciser le "id"
-
 SHOW TABLES;
 
 DESC livre\p;
@@ -26,13 +17,6 @@ LIMIT 20
 
 SELECT 'Combien d''exemplaires sont empruntes en ce moment ?' AS Exercice_2;
 
--- SELECT exemplaire.id, reference, emprunt.date_retour
--- FROM exemplaire
--- INNER JOIN emprunt ON exemplaire.id = emprunt.id
--- WHERE emprunt.date_retour IS NULL
--- ORDER BY exemplaire.id ASC
--- ;
-
 SELECT count(*) AS nombreExemplaire 
 FROM exemplaire
 INNER JOIN emprunt ON exemplaire.id = emprunt.id
@@ -41,11 +25,6 @@ ORDER BY exemplaire.id ASC
 \p;
 
 SELECT 'Quel est le lecteur qui a emprunte le plus de livres ?' AS Exercice_3;
-
--- SELECT nom, prenom, emprunt.lecteur_id
--- FROM lecteur
--- INNER JOIN emprunt ON
--- ;
 
 SELECT lecteur.id, nom, prenom, count(*) AS nombreExemplaire
 FROM lecteur
@@ -57,14 +36,6 @@ LIMIT 1 -- limiter au premier résultat dans le tableau
 
 SELECT 'Quel est le livre qui a ete le plus emprunte ?' AS Exercice_4;
 
--- SELECT exemplaire.id, count(*) AS nombreEmprunt
--- FROM exemplaire
--- INNER JOIN emprunt ON exemplaire.livre_id = emprunt.id
--- GROUP BY exemplaire.id
--- ORDER BY nombreEmprunt DESC
--- LIMIT 20 -- limiter au premier résultat dans le tableau
--- ;
-
 SELECT livre.id, livre.titre, count(*) AS nombreEmprunt
 FROM exemplaire
 INNER JOIN livre ON exemplaire.livre_id = livre.id
@@ -73,3 +44,7 @@ GROUP BY livre.id
 ORDER BY nombreEmprunt DESC
 LIMIT 1
 ;
+
+SELECT 'Afficher la liste des exemplaires empruntés par le lecteur ayant l''ID 1' AS Exercice_5;
+-- On affichera le nom du lecteur, son prénom, le nom du livre, la date d'emprunt ainsi que la date retour.
+
