1a2d6f19b546a92d1ddefbba2d2b72e1b17a4a84
David <dehoustdavid@gmail.com>
2023-01-18 14:50:33 +0100

    ras

--- DB/biblio4_exe.sql
+++ DB/biblio4_exe.sql
@@ -45,6 +45,30 @@ ORDER BY nombreEmprunt DESC
 LIMIT 1
 ;
 
-SELECT 'Afficher la liste des exemplaires empruntés par le lecteur ayant l''ID 1' AS Exercice_5;
+SELECT 'Afficher la liste des exemplaires empruntes par le lecteur ayant l''ID 1' AS Exercice_5;
 -- On affichera le nom du lecteur, son prénom, le nom du livre, la date d'emprunt ainsi que la date retour.
+
+SELECT nom, prenom, titre, date_emprunt, date_retour
+FROM emprunt
+INNER JOIN lecteur ON emprunt.lecteur_id = lecteur.id
+INNER JOIN exemplaire ON emprunt.exemplaire_id = exemplaire.id
+INNER JOIN livre ON exemplaire.livre_id = livre.id
+WHERE emprunt.lecteur_id = 1
+;
+
+SELECT 'Idem mais affichez en plus le nom et le prenom de l''auteur.' AS Exercice_6;
+-- Il faudra faire attention qu'on ne puisse pas hésiter à la lecture si ce sont les noms et prénoms des lecteurs/auteurs.
+
+SELECT lecteur.nom AS nom_lecteur, lecteur.prenom AS prenom_lecteur, auteur.nom A
+FROM emprunt
+INNER JOIN lecteur ON emprunt.lecteur_id = lecteur.id
+INNER JOIN exemplaire ON emprunt.exemplaire_id = exemplaire.id
+INNER JOIN livre ON exemplaire.livre_id = livre.id
+INNER JOIN auteur ON livre.auteur_id = auteur.id
+WHERE emprunt.lecteur_id = 1
+;
+
+SELECT 'Afficher la liste des livres ayant un auteur né avant le 1er janvier 1980.' AS Exercice_7;
+
+
 
