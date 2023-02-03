c6199f9e23b7c7ed6a87a777779d76df6a2b7190
David <dehoustdavid@gmail.com>
2023-02-03 12:14:23 +0100

    update

--- javaObjet/designPatern/ObserverPattern/MyTopic.java
+++ javaObjet/designPatern/ObserverPattern/MyTopic.java
@@ -1,24 +1,60 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class MyTopic implements ISubject{
+/**
+ * MyTopic.
+ */
+
+public class MyTopic implements ISubject {
+
+    // Properties.
+
+    private List<IObserver> Observers;
+    private String message;
+    private boolean hasChanged;
+    /**
+     * MUTEX signifie MUTual EXclusion.
+     * Sert à faire en sorte qu'une ressource ne soit pas accedé par deux éléments en même temps.
+     */
+    private final Object MUTEX = new Object(); // Les constatntes s'écrivent toujours en CapsLock.
+
+    // Constructor.
+
+    public MyTopic() {
+
+        Observers = new ArrayList<IObserver>();
+
+    }
+
+    // Override.
 
     @Override
     public void Register(IObserver _obs) {
-        // TODO Auto-generated method stub
+
+        if (_obs == null) throw new NullPointerException("L'observeur est null.");
         
+        synchronized(MUTEX){
+
+            if (!Observers.contains(_obs)) {
+
+                Observers.add(_obs);
+                
+            }
+
+        }
+
     }
 
     @Override
     public void UnRegister(IObserver _obs) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void NotifyObservers() {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
@@ -26,7 +62,5 @@ public class MyTopic implements ISubject{
         // TODO Auto-generated method stub
         return null;
     }
-    
-
 
 }
